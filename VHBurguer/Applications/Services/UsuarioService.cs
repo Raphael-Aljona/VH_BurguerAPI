5e38a3443c346527fe2ea07d6109a3aaa0286fc9
Raphael-Aljona <raphael.aljona@gmail.com>
2026-02-11 10:03:50 -0300

    First Commit

---
+++ VHBurguer/Applications/Services/UsuarioService.cs
@@ -0,0 +1,39 @@
+﻿using VHBurguer.Domains;
+using VHBurguer.DTOs;
+using VHBurguer.Interfaces;
+
+namespace VHBurguer.Applications.Services
+{
+    public class UsuarioService
+    {
+        // repository é o canal de comunicação para acessar os dados.
+        private readonly IUsuarioRepository _repository;
+
+        // injeção de dependenicias -> implementamos o repositório e o service só depende da interface.
+        public UsuarioService(IUsuarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        private static LerUsuarioDto LerDto(Usuario usuario)
+        {
+            LerUsuarioDto lerUsuario = new LerUsuarioDto
+            {
+                UsuarioID = usuario.UsuarioID,
+                Nome = usuario.Nome,
+                Email = usuario.Email,
+                StatusUsuario = usuario.StatusUsuario ?? true
+            };
+
+            return lerUsuario;
+        }
+
+        public List<LerUsuarioDto> Listar()
+        {
+            List<Usuario> usuarios = _repository.Listar();
+
+            List<LerUsuarioDto> usuariosDto = usuarios.Select(usuarioBanco => LerDto(usuarioBanco)).ToList();
+            return usuariosDto;
+        }
+    }
+}
