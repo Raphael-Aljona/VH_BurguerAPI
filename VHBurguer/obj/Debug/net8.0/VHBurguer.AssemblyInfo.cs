c28342d9ce2ca123efc2ea7d3801142fcb988435
Raphael-Aljona <raphael.aljona@gmail.com>
2026-05-14 08:22:30 -0300

    fix: Correção do get na imagem

--- VHBurguer/obj/Debug/net8.0/VHBurguer.AssemblyInfo.cs
+++ VHBurguer/obj/Debug/net8.0/VHBurguer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VHBurguer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a85490a167a4a526078101e3ef36cd7b339c0ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d698954779fca8610c8bdfbf40fc2a8978ef97a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("VHBurguer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VHBurguer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
